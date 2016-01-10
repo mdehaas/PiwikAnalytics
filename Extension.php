0549c5a658031583f952f27ef2a8d10cdbd02d27
mdehaas <marcel@bommelhaas.nl>
2016-01-10 23:14:52 +0100

    Discovering the plugin model.

--- Extension.php
+++ Extension.php
@@ -11,10 +11,20 @@ class Extension extends BaseExtension
     {
         $this->addCss('assets/extension.css');
         $this->addJavascript('assets/start.js', true);
+		
+		#Add the Piwik tracking code at the end of the body
+		$this->addSnippet('endofbody', 'insertPiwikTracking');
     }
 
     public function getName()
     {
         return "Piwik Analytics";
     }
+	
+	public function insertPiwikTracking{}
+	{
+		$piwikSnippet = '%piwikurl% - %piwiksiteid%'
+		$piwikSnippet = str_replace("", this->config['piwikurl'], $piwikSnippet);
+		return new \Twik_Markup($piwikSnippet, 'UTF-8'
+	}
 }
